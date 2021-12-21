699ae398a0157edd7d9bb938d8ecf65e5cf46d40
lmxdawn <lmxdawn@qq.com>
2021-12-21 21:51:11 +0800

    优化首页

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -22,13 +22,28 @@
     </uni-nav-bar>
 
     <view class="content">
-      <image class="logo" src="/static/logo.png"></image>
-      <view class="text-area">
-        <text class="title">{{title}}</text>
+
+      <view class="banner">
+        <uni-swiper-dot :info="adList" :current="adCurrent" field="content" :mode="adMode" :dotsStyles="{backgroundColor: '#68B1C0', selectedBackgroundColor: '#FFFFFF'}">
+          <swiper class="swiper-box" @change="adChange">
+            <swiper-item v-for="(item ,index) in adList" :key="index">
+              <view class="swiper-item">
+                <image :src="item"></image>
+              </view>
+            </swiper-item>
+          </swiper>
+        </uni-swiper-dot>
       </view>
-      <view>
-        <u-icon name="photo" color="#2979ff" size="50"></u-icon>
+
+      <view class="card">
+        <view class="card-head">
+          <u-notice-bar :text="noticeList" bgColor="#191E29" color="#E1E8F5" direction="column" mode="link" speed="250" url="/pages/componentsB/tag/tag"></u-notice-bar>
+        </view>
+        <view class="card-body">
+
+        </view>
       </view>
+
     </view>
   </view>
 </template>
@@ -38,13 +53,27 @@
 		data() {
 			return {
 				title: this.$t('common.home'),
-        value: 1
+        value: 1,
+        adCurrent: 0,
+        adMode: 'round',
+        adList: [
+          'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+          'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+          'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+        ],
+        noticeList: [
+            "sfsdafsafsdfds",
+            "光大发的顺丰顺丰大发顺丰萨达地方撒范德萨发神鼎飞丹砂烦都烦死神鼎飞丹砂方方达打多少分",
+        ]
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
+      adChange(e) {
+        this.adCurrent = e.detail.current;
+      },
       message() {
 
       },
@@ -59,10 +88,6 @@
 </script>
 
 <style lang="scss">
-	.content {
-    height: 1500rpx;
-	}
-
   $nav-height: 30px;
 
   .uni-nav-bar-text {
@@ -106,4 +131,46 @@
     padding: 0 5px;
     font-size: 14px;
   }
+
+  .content {
+    background-color: #212631;
+    height: 1500rpx;
+  }
+  .banner {
+    background-color: #212631;
+    padding: 15px 0 20px;
+  }
+  .swiper-item {
+    padding: 0 22px;
+  }
+
+  .card{
+    background-color: #191E29;
+    .card-head {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 40px;
+      border-bottom: 1px solid #292E39;
+      padding: 5px 0;
+      border-radius-topleft: 5px;
+      .new-box {
+        font-size: 13px;
+        padding: 0 8px;
+        text-align: left;
+        flex: 1;
+        color: #E1E8F5;
+        overflow:hidden;
+        .new-title {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      }
+    }
+    .card-body {
+      min-height: 10px;
+    }
+  }
+
 </style>
