f85cf9196f559301a4e4a3bae9d6744ea6223791
lmxdawn <lmxdawn@qq.com>
2021-12-25 20:38:29 +0800

    优化代码

--- pages/trade/index.vue
+++ pages/trade/index.vue
@@ -1,17 +1,15 @@
 <template>
-	<view class="content">
-    <view class="box" :class="[go ? 'transparent-up' : '']"></view>
-    <view class="box" :class="[go ? 'transparent-down' : '']"></view>
-    <my-empty></my-empty>
+	<view class="page-box">
+
 	</view>
 </template>
 
 <script>
   import {mapActions, mapGetters} from "vuex";
-  import myEmpty from "../../components/my-empty/my-empty"
+  import myNavBarTrade from "../../components/my-nav-bar/my-nav-bar-trade"
   export default {
     components: {
-      myEmpty
+      myNavBarTrade
     },
     computed: {
       ...mapGetters({
@@ -21,35 +19,13 @@
 		data() {
 			return {
 				title: this.$t('common.trade'),
-        go: false
 			}
 		},
 		onLoad() {
-      // 监听事件
-      uni.$on('matchWs',(obj)=>{
-        console.log(obj)
-      })
-      this.usdtRateSet()
-      this.marketCollectSet(1)
-      setTimeout(() => {
-        this.go = true
-        let item = {
-          tradeCoinId: 1,
-          coinId: 3,
-          price: 3.311,
-          amount: 2,
-        }
-        uni.$emit('matchWs', item)
 
-        setTimeout(() => {
-          this.go = false
-        }, 500)
-
-      }, 2 * 1000)
 		},
     onUnload() {
-      // 移除撮合监听事件
-      uni.$off('matchWs');
+
     },
 		methods: {
       ...mapActions({
@@ -60,25 +36,7 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style scoped>
+	.page-box {
 	}
-  .box {
-    height: 200rpx;
-    width: 200rpx;
-    background-color: transparent;
-    transition-property: background-color;
-    transition-duration: 1s
-  }
-
-  .transparent-up {
-    background-color: #1A272F;
-  }
-  .transparent-down {
-    background-color: #24222C;
-  }
 </style>
